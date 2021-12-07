ff6fca8379ceacec24e8e7d8137d28e4c0c17716
Dendy Pratama <s160419122@student.ubaya.ac.id>
2021-12-07 14:56:23 +0700

    tes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->enum('roles', ['customer', 'seller', 'administrator'])->default('customer');
+            $table->enum('suspend', ['suspend', 'unsusped'])->default('unsuspend');
             $table->rememberToken();
             $table->timestamps();
         });
